d0aea5744f503ac4abd382298e4b3d85aa8567b3
Reza Saker <saker.reza001@hotmail.com>
2019-04-08 01:09:05 +0600

    some general modification

--- MyCost/Server Scripts/connectDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-DEFINE('SERVER', 'server_address');
-DEFINE('USERNAME', 'username');
-DEFINE('PASSWORD', 'password');
-DEFINE('DATABASE', 'Database_name');
-
-$connectDB = mysqli_connect(SERVER, USERNAME, PASSWORD, DATABASE) or die("SERVER_ERROR");
-
-?>
