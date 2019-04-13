4d4e9145e4b5f6532c3ce1df73a475b7f57880f9
Reza Saker <saker.reza001@hotmail.com>
2019-04-13 08:23:58 +0600

    modified

--- MyCost API/connectDB.php
+++ MyCost API/connectDB.php
@@ -5,10 +5,10 @@
 	*this file is used by several other files
 */
 
-DEFINE('SERVER', 'localhost');
-DEFINE('USERNAME', 'root');
-DEFINE('PASSWORD', 'mysql1997');
-DEFINE('DATABASE', 'mycost');
+DEFINE('SERVER', 'server address');
+DEFINE('USERNAME', 'username');
+DEFINE('PASSWORD', 'password');
+DEFINE('DATABASE', 'database name');
 
 $connect = mysqli_connect(SERVER, USERNAME, PASSWORD, DATABASE) or die('Server connection error');
 
